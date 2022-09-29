36762c401701d4ca1413c03887257f537b7a2b51
Nelfyk <ruslan-burduzhan@mail.ru>
2022-09-29 12:54:29 +0500

    added drawing tile and player. added reading world map from txt file

---
+++ src/entity/Player.java
@@ -0,0 +1,104 @@
+package entity;
+
+import main.GamePanel;
+import main.KeyHandler;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Player extends Entity {
+
+    GamePanel gp;
+    KeyHandler keyH;
+
+    public final int screenX;
+    public final int screenY;
+
+    public Player(GamePanel gp, KeyHandler keyH) {
+        this.gp = gp;
+        this.keyH = keyH;
+
+        screenX = gp.screenWidth / 2 - (gp.tileSize / 2);
+        screenY = gp.screenHeight / 2 - (gp.tileSize / 2);
+
+        setDefault();
+        getPlayerImage();
+    }
+
+    public void setDefault() {
+        worldX = gp.tileSize * 23;
+        worldY = gp.tileSize * 21;
+        speed = 4;
+        direction = "down";
+    }
+
+    public void getPlayerImage() {
+        try {
+            left1 = ImageIO.read(getClass().getResourceAsStream(("/player/boy_left_1.png")));
+            left2 = ImageIO.read(getClass().getResourceAsStream(("/player/boy_left_2.png")));
+            up1 = ImageIO.read(getClass().getResourceAsStream(("/player/boy_up_1.png")));
+            up2 = ImageIO.read(getClass().getResourceAsStream(("/player/boy_up_2.png")));
+            right1 = ImageIO.read(getClass().getResourceAsStream(("/player/boy_right_1.png")));
+            right2 = ImageIO.read(getClass().getResourceAsStream(("/player/boy_right_2.png")));
+            down1 = ImageIO.read(getClass().getResourceAsStream(("/player/boy_down_1.png")));
+            down2 = ImageIO.read(getClass().getResourceAsStream(("/player/boy_down_2.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update() {
+        if (keyH.leftPressed || keyH.upPressed || keyH.rightPressed || keyH.downPressed) {
+            if (keyH.leftPressed) {
+                direction = "left";
+                worldX -= speed;
+            } else if (keyH.upPressed) {
+                direction = "up";
+                worldY -= speed;
+            } else if (keyH.rightPressed) {
+                direction = "right";
+                worldX += speed;
+            } else if (keyH.downPressed) {
+                direction = "down";
+                worldY += speed;
+            }
+            spriteCounter++;
+            if (spriteCounter > 12) {
+                if (spriteNum == 1) {
+                    spriteNum = 2;
+                } else spriteNum = 1;
+                spriteCounter = 0;
+            }
+        }
+    }
+
+    public void draw(Graphics2D g2) {
+        BufferedImage image = null;
+
+        switch (direction) {
+            case "left" -> {
+                if (spriteNum == 1) {
+                    image = left1;
+                } else image = left2;
+            }
+            case "up" -> {
+                if (spriteNum == 1) {
+                    image = up1;
+                } else image = up2;
+            }
+            case "right" -> {
+                if (spriteNum == 1) {
+                    image = right1;
+                } else image = right2;
+            }
+            case "down" -> {
+                if (spriteNum == 1) {
+                    image = down1;
+                } else image = down2;
+            }
+        }
+        g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+    }
+}
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,32 +1,39 @@
 package main;
 
+import entity.Player;
+import tile.TileManager;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel implements Runnable{
+public class GamePanel extends JPanel implements Runnable {
 
     //SCREEN SETTINGS
     final int originalTileSize = 16; // 16x16 tile
     final int scale = 3;
 
-    final int tileSize = originalTileSize*scale; // 48x48 tile
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = maxScreenCol * tileSize; // 768 pixels
-    final int screenHeight = maxScreenRow * tileSize; // 576 pixels
+    public final int tileSize = originalTileSize * scale; // 48x48 tile
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = maxScreenCol * tileSize; // 768 pixels
+    public final int screenHeight = maxScreenRow * tileSize; // 576 pixels
+    //WORLD SETTINGS
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldWith = tileSize * maxWorldCol; // 2400 pixels
+    public final int worldHeight = tileSize * maxWorldRow; // 2400 pixels
+
     //FPS
     final int FPS = 60;
 
     Thread gameThread;
     KeyHandler keyH = new KeyHandler();
+    TileManager tileM = new TileManager(this);
+    public Player player = new Player(this, keyH);
 
-    // Set player default position
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
 
-    public GamePanel(){
-        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setFocusable(true);
         this.setBackground(Color.black);
         this.addKeyListener(keyH);
@@ -34,47 +41,39 @@ public class GamePanel extends JPanel implements Runnable{
         startGameThread();
     }
 
-    public void startGameThread(){
+    public void startGameThread() {
         gameThread = new Thread(this);
         gameThread.start();
     }
 
     @Override // !!!GAME LOOP!!! Option #2. ("Delta/Accumulator" method)
     public void run() {
-        double drawInterval=1000000000/FPS; // 0.1666 seconds
-        double delta=0;
-        long lastTime=System.nanoTime();
+        double drawInterval = 1000000000 / FPS; // 0.1666 seconds
+        double delta = 0;
+        long lastTime = System.nanoTime();
         long currentTime;
-        while (gameThread!=null){        //пока поток существует
-            currentTime=System.nanoTime();
-            delta+=(currentTime-lastTime)/drawInterval;
-            lastTime=currentTime;
-            if(delta>=1){
+        while (gameThread != null) {        //пока поток существует
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+            if (delta >= 1) {
                 update(); // 1 UPDATE: update information such as character positions
                 repaint(); // 2 DRAW: draw the screen with the update information
                 delta--;
-                System.out.println();
             }
         }
     }
-    public void update(){
-        if(keyH.leftPressed){
-            playerX-=playerSpeed;
-        } else if(keyH.upPressed){
-            playerY-=playerSpeed;
-        } else if(keyH.rightPressed){
-            playerX+=playerSpeed;
-        } else if(keyH.downPressed){
-            playerY+=playerSpeed;
-        }
-    }
-    @Override
-    public void paint(Graphics g){
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D)g;
 
-        g2.setColor(Color.white);
-        g2.fillRect(playerX,playerY,tileSize,tileSize);
+    public void update() {
+        player.update();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        tileM.draw(g2);
+        player.draw(g2);
         g2.dispose();
     }
 }
